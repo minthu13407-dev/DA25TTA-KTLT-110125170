74475287366b8286a9a76bde8252dbeab83a5d9e
minthu13407-dev <minthu13407@gmail.com>
2026-01-28 23:34:11 +0700

    Add files via upload

---
+++ 110125170.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main ()
+{
+	int a,b;
+	 printf("nhap vao a");
+	 scanf("%d",&a);
+	 printf("nhap vao b");
+	 scanf("%d",&b);
+	  
+	  
+	printf("\ntong 2 so nguyen la %d ",a+b);
+		printf("\nhieu 2 so nguyen la %d ",a-b);
+			printf("\ntich 2 so nguyen la %d ",a*b);
+				printf("\nthuong 2 so nguyen la %.2f ",(float)a/b);
+		
+		
+	return 0;
+	}
